556645ed5cd7d2eba59bcdf41c1ee7dd6d5b0222
aneldjesevic <117862081+aneldjesevic@users.noreply.github.com>
2023-03-04 20:37:48 +0100

    update

--- Game Jam/Assets/Scripts/CameraShake.cs
+++ Game Jam/Assets/Scripts/CameraShake.cs
@@ -6,6 +6,7 @@ public class CameraShake : MonoBehaviour
 {
     CameraFollow camFollow;
 
+
     private void Start()
     {
         camFollow = FindObjectOfType<CameraFollow>();
@@ -22,20 +23,23 @@ public class CameraShake : MonoBehaviour
 
         int shakeTimes = Random.Range(4, 8);
 
-        for (int i = 0; i <= shakeTimes; i++)
+        Vector3 originalPosition = transform.position;
+
+        for (int i = 0; i < shakeTimes; i++)
         {
             float randomTime = Random.Range(0.05f, 0.15f);
 
+            Vector3 randomOffset = new Vector3(Random.Range(-1, 2), Random.Range(-1, 2), 0);
+
+            transform.position += randomOffset;
+
             yield return new WaitForSeconds(randomTime);
 
-            int randomX = Random.Range(-1, 2);
-            int randomY = Random.Range(-1, 2);
-
-            transform.position = new Vector3(transform.position.x + randomX, transform.position.y + randomY, -10);
+            transform.position = originalPosition;
         }
 
         yield return new WaitForSeconds(0.5f);
 
-        camFollow.enabled = false;
+        camFollow.enabled = true;
     }
 }
